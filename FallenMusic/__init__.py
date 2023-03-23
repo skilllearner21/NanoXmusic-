80e6c5d3a15fd0ba212c3d7fe25cfff89b2343a2
Learning Bots <115718228+Learningbots79@users.noreply.github.com>
2023-03-23 08:48:06 +0530

    Update __init__.py

--- FallenMusic/__init__.py
+++ FallenMusic/__init__.py
@@ -92,8 +92,8 @@ async def fallen_startup():
     ASS_USERNAME = getme2.username
     ASS_MENTION = getme2.mention
     try:
-        await app2.join_chat("DevilsHeavenMF")
-        await app2.join_chat("FallenAssociation")
+        await app2.join_chat("learningbots79")
+        await app2.join_chat("official_learning_bots_Support")
     except:
         pass
 
